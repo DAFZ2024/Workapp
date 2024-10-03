b322b2c9e48085e5a1b4c3724f99cadb633429a8
DAFZ2000 <du28fo@gmail.com>
2024-10-03 12:47:09 -0500

    cambio pequeño

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ Create a new file called build.sh in your project’s root directory and paste i
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
+#pip install -r requirements.txt
 
 # Convert static asset files
 python manage.py collectstatic --no-input
